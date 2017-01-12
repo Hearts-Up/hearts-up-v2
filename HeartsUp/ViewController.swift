8fdb48be7c5af425357bfc87ed444b66e04fa3f2
Elijah SquidThePrinter <squidtheprinter@gmail.com>
2017-01-11 21:16:41 -0500

    Initial Commit

---
+++ HeartsUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeartsUp
+//
+//  Created by Elijah Smith on 1/11/17.
+//  Copyright © 2017 HeartsUp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
